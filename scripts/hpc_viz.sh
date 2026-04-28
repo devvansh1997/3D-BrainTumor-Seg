c1c62b9973e5b00a57410488d121a8e06244c2cc
Devansh Sharma <devansh.sharma1997@gmail.com>
2026-04-28 11:16:12 -0400

    viz hpc

--- scripts/hpc_viz.sh
+++ scripts/hpc_viz.sh
@@ -4,11 +4,11 @@
 #SBATCH --partition=normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --gpus=1
 #SBATCH --constraint=gpu80
-#SBATCH --mem=32G
-#SBATCH --time=00:30:00
+#SBATCH --mem=128G
+#SBATCH --time=24:00:00
 #SBATCH --output=slurm_logs/%j/slurm_%j.out
 #SBATCH --error=slurm_logs/%j/slurm_%j.err
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -20,9 +20,11 @@
 mkdir -p slurm_logs/$SLURM_JOB_ID
 
 echo "Job started at $(date)" >> slurm_logs/$SLURM_JOB_ID/slurm_${SLURM_JOB_ID}.out
+echo "Generating segmentation visualisations" >> slurm_logs/$SLURM_JOB_ID/slurm_${SLURM_JOB_ID}.out
 
 # load modules
 module load anaconda/anaconda-2023.09 cuda/cuda-12.6.0
+echo "Following modules loaded..." >> slurm_logs/$SLURM_JOB_ID/slurm_${SLURM_JOB_ID}.err
 module list >> slurm_logs/$SLURM_JOB_ID/slurm_${SLURM_JOB_ID}.err
 
 # activate env
@@ -35,8 +37,10 @@ cd /lustre/fs1/home/de807845/med_img_computing/3D-BrainTumor-Seg/
 echo "Current directory: $(pwd)" >> slurm_logs/$SLURM_JOB_ID/slurm_${SLURM_JOB_ID}.out
 
 # run viz
-echo "Generating segmentation visualisations..."
-python scripts/visualize_seg.py configs/dynunet_hpc.yaml --num 3
+echo "Starting visualisation"
+python scripts/visualize_seg.py configs/dynunet_hpc.yaml --num 2
 
+# deactivate
 conda deactivate
+echo "Virtual environment deactivated"
 echo "Run completed!"
